bb78aa2daabf03808ee9b6125b9a6406bd48e74f
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-05-10 14:59:14 -0600

    Pause Menu y Casi Main Manue

---
+++ Assets/Code/Manager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+
+    // Menu de pausa
+    public GameObject pauseMenu;
+    public GameObject fog;
+    private bool active = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Pausa
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        if (pauseMenu)
+        {
+            fog.SetActive(!pauseMenu.activeSelf);
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            active = !active;
+            Time.timeScale = active ? 0.0f : 1.0f;
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ChangeScene(int indexScene)
+    {
+        SceneManager.LoadScene(indexScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
