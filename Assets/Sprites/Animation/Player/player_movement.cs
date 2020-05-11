2c94f0852118be13fa5d8a3ba7652e5c3c39282f
andreamalin <andreaa.malin@gmail.com>
2020-05-11 12:48:33 -0600

    Se termino el main y los scripts de animacion

---
+++ Assets/Sprites/Animation/Player/player_movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public Animator animator;
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+    public LayerMask groundLayer;
+
+    // Update is called once per frame
+    private void Start()
+    {
+
+    }
+    void Update()
+    {
+        //Se obtiene la velocidad en x (y no se necesitara)
+        movement.x = Input.GetAxisRaw("Horizontal");
+        //Se cambia la animacion
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
+    }
+    void FixedUpdate()
+    {
+        //Se mueve
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        //Raycast para cuando salte
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.25f, groundLayer);
+
+        //Para cuando salte
+        if (hit.collider != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector2.up * 20f, ForceMode2D.Impulse);
+            }
+        }
+
+    }
+}
---
+++ Assets/Sprites/Animation/Zombie/zombie_movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie_movement : MonoBehaviour
+{
+    public LayerMask enemyLayer;
+    private bool right = true;
+    public float distance;
+    public float speed;
+    private Transform tf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>(); 
+    }
+
+    void FixedUpdate()
+    {
+        //Para cambiar su posicion
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D enemy = Physics2D.Raycast(tf.position, Vector2.down, distance, enemyLayer);
+        //Se revisa que este en su layer
+        if (enemy.collider == false)
+        {
+            if (right == true)
+            { //Derecha
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                right = false;
+            }
+            else
+            { //Izquierda
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                right = true;
+            }
+        }
+    }
+}
