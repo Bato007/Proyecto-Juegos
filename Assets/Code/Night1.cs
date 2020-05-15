1ac6c9d7411baf25a81d9f0bee5c7a5b28c16010
andreamalin <andreaa.malin@gmail.com>
2020-05-14 19:08:43 -0600

    se trabajo la noche 1

---
+++ Assets/Code/Night1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Night1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Se escucha el sonido del zombie en la ventana
+        if (collision.gameObject.CompareTag("ZombieFX"))
+        {
+            collision.gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+}
--- Assets/Sprites/Animation/Player/player_movement.cs
+++ Assets/Sprites/Animation/Player/player_movement.cs
@@ -9,6 +9,7 @@ public class player_movement : MonoBehaviour
     public Rigidbody2D rb;
     private Vector2 movement;
     public LayerMask groundLayer;
+    private bool isJumping = false;
 
     // Update is called once per frame
     private void Start()
@@ -19,16 +20,34 @@ public class player_movement : MonoBehaviour
     {
         //Se obtiene la velocidad en x (y no se necesitara)
         movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //Se cambia la escala del personaje dependiendo de la direccion a la que vaya
+        Vector3 charScale = transform.localScale;
+        if (movement.x == -1)
+        {
+            charScale.x = -0.1f;
+        }
+        if (movement.x == 1)
+        {
+            charScale.x = 0.1f;
+        }
+        transform.localScale = charScale;
+
+
         //Se cambia la animacion
         animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
-
-
     }
     void FixedUpdate()
     {
-        //Se mueve
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        //Se mueve si no es salto
+        if(movement.y != 1)
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+        
 
         //Raycast para cuando salte
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.25f, groundLayer);
@@ -36,11 +55,24 @@ public class player_movement : MonoBehaviour
         //Para cuando salte
         if (hit.collider != null)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) || movement.y == 1)
             {
-                rb.AddForce(Vector2.up * 20f, ForceMode2D.Impulse);
+                isJumping = true;
+                rb.AddForce(Vector2.up * 1f, ForceMode2D.Impulse);
             }
         }
 
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Se revisa que haya intentado saltar, de lo contrario se ignora
+        if (isJumping)
+        {
+            print("se hace");
+        } else
+        {
+            Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), other.GetComponent<Collider2D>());
+        }
+    }
+
 }
