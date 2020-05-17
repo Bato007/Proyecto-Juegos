cb5ea46a014c356658fc5d75b32439634977d6ab
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-05-17 08:30:23 -0600

    Ahora se puede ir de la escena 1 a la 2

---
+++ Assets/Code/Camp.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camp : MonoBehaviour
+{
+
+    static private int campHP = 0;
+    public GameObject box_trigger;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+               
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+
+        // Si es el player lo deja pasar
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
+        }
+
+        // Si es un zombie lo manda a volar y se quita vida
+        else
+        {
+            campHP -= 60;
+            collision.gameObject.transform.position = new Vector2(1.55f, -2.41f);
+            if (campHP < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            campHP = 80;
+            showCamp();
+            collision.gameObject.transform.position = new Vector2(1.55f, -2.41f);
+
+            box_trigger.GetComponent<BoxCollider2D>().enabled = true;
+        }
+       
+
+    }
+
+    private void showCamp()
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+    }
+
+}
---
+++ Assets/Code/Inventory_Box.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory_Box : MonoBehaviour
+{
+
+    public GameObject nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        nextScene.GetComponent<BoxCollider2D>().enabled = true;
+    }
+}
