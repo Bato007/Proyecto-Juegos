c4a1ef54d89d92d997e17da852ae8eb1bc8c04fd
andreamalin <andreaa.malin@gmail.com>
2020-05-28 12:17:59 -0600

    Agregue cuadro de dialogo y menus a las escenas que hay

---
+++ Assets/Code/Dialogo/DialogueManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager:MonoBehaviour
+{
+    private Queue<string> sentences;
+    public Text dialogueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+
+
+}
---
+++ Assets/Code/Main.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    public AudioSource ambiental;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ambiental.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(int indexScene)
+    {
+        SceneManager.LoadScene(indexScene);
+    }
+
+}
