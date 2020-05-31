3712f8a2ea7b5f26be9d83990489176dbfa45e77
Requetetin <martinamadog@gmail.com>
2020-05-31 17:59:46 -0600

    final?

--- Assets/Code/ColorChange.cs
+++ Assets/Code/ColorChange.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ColorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
@@ -28,6 +29,8 @@ public class ColorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     {
         
     }
+
+   
 
     
 }
---
+++ Assets/GoToTitle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToTitle : MonoBehaviour
+{
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TitleChange(int done)
+    {
+        if (done == 1)
+            SceneManager.LoadScene(0);
+    }
+    public void Congratulations()
+    {
+        text.SetActive(true);
+    }
+}
